34d733af96ded989597a5556540e37b19d173c6f
Aleksei Taraskin <bakamoena@gmail.com>
2023-10-26 05:43:57 +0700

    init

---
+++ src/app/shared/components/applicability-section/applicability-section.component.ts
@@ -0,0 +1,164 @@
+import {
+    ChangeDetectionStrategy,
+    ChangeDetectorRef,
+    Component,
+    HostListener, Inject,
+    OnInit,
+    ViewEncapsulation,
+    PLATFORM_ID,
+} from '@angular/core';
+import { BaseComponent } from '@core/classes/base-component';
+import Swiper from 'swiper';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
+import EffectCarousel from './effect-carousel.esm.js';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+//@ts-ignore
+import EffectCarouselLarge from './effect-carousel-large.esm.js';
+import { DATA } from './data';
+import { isPlatformBrowser } from '@angular/common';
+import { Autoplay } from 'swiper/modules';
+
+@Component({
+    selector: 'owner-applicability-section',
+    templateUrl: './applicability-section.component.html',
+    styleUrls: ['./applicability-section.component.scss'],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    encapsulation: ViewEncapsulation.None,
+
+})
+export class ApplicabilitySectionComponent extends BaseComponent implements OnInit {
+
+
+    public groups: any = DATA;
+
+    public slider: any;
+    public sliderDilay: any;
+    public sliderInterval: any;
+
+    public activeGroupIndex = 0;
+    public activePadeIndex = 0;
+    public currentConfig: any;
+    public caruselItems: any = [];
+
+    public constructor(public cdr: ChangeDetectorRef, @Inject(PLATFORM_ID) private _platformId: string) {
+        super();
+        this._detectScreenSize();
+    }
+
+    @HostListener('window:resize', ['$event'])
+    public onResize(): void {
+        this._detectScreenSize();
+    }
+
+    private _detectScreenSize(): void {
+        if (isPlatformBrowser(this._platformId)) {
+            const windowWidth = window.innerWidth;
+            if (windowWidth > 1280) {
+                this.currentConfig = EffectCarouselLarge;
+            } else {
+                this.currentConfig = EffectCarousel;
+            }
+        }
+
+    }
+
+    public ngOnInit(): void {
+        if (typeof document === 'undefined') return;
+        this._initSwiper();
+    }
+
+
+    private _initSwiper(): void {
+        for (let i = 0; i < this.groups.length; i++) {
+            const groupIndex = i;
+            for (let j = 0; j < this.groups[groupIndex].items.length; j++) {
+                this.caruselItems.push({ ...this.groups[groupIndex].items[j], ...{ groupIndex } });
+            }
+        }
+        this.slider = new Swiper('.swiper', {
+            loop: true,
+            modules: [this.currentConfig, Autoplay],
+            effect: 'carousel',
+            slidesPerView: 'auto',
+            shortSwipes: true,
+            centeredSlides: true,
+            grabCursor: false,
+            speed: 500,
+            resizeObserver: true,
+            slideToClickedSlide: true,
+        });
+        this.slider.on('slideChange', (e: any): void => {
+            this.activePadeIndex = e.realIndex;
+            if (this.caruselItems[this.activePadeIndex].groupIndex !== this.activeGroupIndex) {
+                this.activeGroupIndex = this.caruselItems[this.activePadeIndex].groupIndex;
+                this._toggleAnimation();
+            }
+            this.cdr.detectChanges();
+        });
+
+        this._setSliderInterval(3000);
+
+    }
+
+
+    private _setSliderInterval(delay: number): void {
+        if (this.sliderDilay !== delay) {
+            if (this.sliderInterval) {
+                clearInterval(this.sliderInterval);
+            }
+
+            this.sliderInterval = setInterval(() => {
+                const nextIndex = this.activePadeIndex + 1;
+                if (nextIndex < this.caruselItems.length) {
+                    this.slider.slideTo(nextIndex);
+                } else {
+                    this.slider.slideTo(0);
+                }
+
+            }, delay);
+
+
+            this.sliderDilay = delay;
+        }
+
+    }
+
+
+    // Function to change the animation state
+    private _toggleAnimation(): void {
+        const element = document.querySelector('.carusel-section');
+        const keyframes = [
+            { opacity: 0 },
+            { opacity: 1 },
+        ];
+        if (element) {
+            element.animate(keyframes, 300);
+        }
+    }
+
+    public mouseEnter(): void {
+        this._setSliderInterval(6000);
+    }
+
+    public mouseLeave(): void {
+        this._setSliderInterval(3000);
+    }
+
+    public changeActiveSlider(index: number): void {
+        if (index === this.activeGroupIndex) {
+            return;
+        }
+        this.activeGroupIndex = index;
+        const itemIndex = this.caruselItems.findIndex((x: any) => x.groupIndex === this.activeGroupIndex);
+        this.paginate(itemIndex);
+        this._toggleAnimation();
+    }
+
+    public paginate(index: any): void {
+        this.slider.slideTo(index);
+        this.cdr.detectChanges();
+        this.sliderDilay = 0;
+        this._setSliderInterval(3000);
+    }
+}
---
+++ src/app/shared/components/applicability-section/data.ts
@@ -0,0 +1,412 @@
+
+export const DATA = [
+    {
+        title: 'Производство',
+        name: 'production',
+        items: [
+            {
+                title: 'Частному сектору 0',
+                items: [
+                    'Удобный учет и контроль имущества граждан',
+                    'Контроль собственных активов и расходов',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/footer-slide-1-colored.png',
+            },
+            {
+                title: 'Гос. учреждениям 1',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-2-colored.png',
+            },
+            {
+                title: 'Гос. учреждениям 2',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-3-colored.png',
+            },
+            {
+                title: 'Гос. учреждениям 3',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/footer-slide-1-colored.png',
+            },
+            {
+                title: 'Гос. учреждениям 4',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/footer-slide-2-colored.png',
+            }
+
+        ],
+    },
+    {
+        title: 'Стройком',
+        name: 'construction',
+        items: [
+            {
+                title: '2 Частному сектору 1',
+                items: [
+                    'Удобный учет и контроль имущества граждан',
+                    'Контроль собственных активов и расходов',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-1-colored.png',
+            },
+            {
+                title: '2 Гос. учреждениям 1',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-2-colored.png',
+            },
+            {
+                title: '2 Гос. учреждениям 2',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-2-colored.png',
+            },
+            {
+                title: '2 Гос. учреждениям 3',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-3-colored.png',
+            },
+            {
+                title: '2 Гос. учреждениям 4',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/footer-slide-1-colored.png',
+            }
+        ],
+    },
+    {
+        title: 'Рынок услуг',
+        name: 'construction',
+        items: [
+            {
+                title: '3 Частному сектору 1',
+                items: [
+                    'Удобный учет и контроль имущества граждан',
+                    'Контроль собственных активов и расходов',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-1-colored.png',
+            },
+            {
+                title: '3 Гос. учреждениям 1',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-2-colored.png',
+            },
+            {
+                title: '3 Гос. учреждениям 2',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-2-colored.png',
+            },
+            {
+                title: '3 Гос. учреждениям 3',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-3-colored.png',
+            },
+            {
+                title: '3 Гос. учреждениям 4',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/footer-slide-1-colored.png',
+            }
+        ],
+    },
+    {
+        title: 'Медиа',
+        name: 'construction',
+        items: [
+            {
+                title: '4 Частному сектору 1',
+                items: [
+                    'Удобный учет и контроль имущества граждан',
+                    'Контроль собственных активов и расходов',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-1-colored.png',
+            },
+            {
+                title: '4 Гос. учреждениям 1',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-2-colored.png',
+            },
+            {
+                title: '4 Гос. учреждениям 2',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-2-colored.png',
+            },
+            {
+                title: '4 Гос. учреждениям 3',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-3-colored.png',
+            },
+            {
+                title: '4 Гос. учреждениям 4',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/footer-slide-1-colored.png',
+            }
+        ],
+    },
+    {
+        title: 'Коммуникации',
+        name: 'construction',
+        items: [
+            {
+                title: '5 Частному сектору 1',
+                items: [
+                    'Удобный учет и контроль имущества граждан',
+                    'Контроль собственных активов и расходов',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-1-colored.png',
+            },
+            {
+                title: '5 Гос. учреждениям 1',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-2-colored.png',
+            },
+            {
+                title: '5 Гос. учреждениям 2',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-2-colored.png',
+            },
+            {
+                title: '5 Гос. учреждениям 3',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-3-colored.png',
+            },
+            {
+                title: '5 Гос. учреждениям 4',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/footer-slide-1-colored.png',
+            }
+        ],
+    },
+    {
+        title: 'Гос. сектор',
+        name: 'construction',
+        items: [
+            {
+                title: '6 Частному сектору 1',
+                items: [
+                    'Удобный учет и контроль имущества граждан',
+                    'Контроль собственных активов и расходов',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-1-colored.png',
+            },
+            {
+                title: '6 Гос. учреждениям 1',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-2-colored.png',
+            },
+            {
+                title: '6 Гос. учреждениям 2',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-2-colored.png',
+            },
+            {
+                title: '6 Гос. учреждениям 3',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-3-colored.png',
+            },
+            {
+                title: '6 Гос. учреждениям 4',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/footer-slide-1-colored.png',
+            }
+        ],
+    },
+    {
+        title: 'ВПК',
+        name: 'construction',
+        items: [
+            {
+                title: '7 Частному сектору 1',
+                items: [
+                    'Удобный учет и контроль имущества граждан',
+                    'Контроль собственных активов и расходов',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-1-colored.png',
+            },
+            {
+                title: '7 Гос. учреждениям 1',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-2-colored.png',
+            },
+            {
+                title: '7 Гос. учреждениям 2',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-2-colored.png',
+            },
+            {
+                title: '7 Гос. учреждениям 3',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-3-colored.png',
+            },
+            {
+                title: '7 Гос. учреждениям 4',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/footer-slide-1-colored.png',
+            }
+        ],
+    },
+    {
+        title: 'Другие',
+        name: 'construction',
+        items: [
+            {
+                title: '8 Частному сектору 1',
+                items: [
+                    'Удобный учет и контроль имущества граждан',
+                    'Контроль собственных активов и расходов',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-1-colored.png',
+            },
+            {
+                title: '8 Гос. учреждениям 1',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-2-colored.png',
+            },
+            {
+                title: '8 Гос. учреждениям 2',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-2-colored.png',
+            },
+            {
+                title: '8 Гос. учреждениям 3',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/project-part-item-3-colored.png',
+            },
+            {
+                title: '8 Гос. учреждениям 4',
+                items: [
+                    'Контроль муниципального имущества',
+                    'Отслеживание выделяемого бюджета',
+                ],
+                phoneImgUrl: 'assets/images/common/Frame-40919.png',
+                bgUrl: 'assets/images/common/footer-slide-1-colored.png',
+            }
+        ],
+    }
+];
