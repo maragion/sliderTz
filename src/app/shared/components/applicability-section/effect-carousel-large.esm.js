925daadc62d4b255c1002998a2810d2206a04c07
Aleksei Taraskin <bakamoena@gmail.com>
2023-11-03 08:41:43 +0700

    fix positions for slides

--- src/app/shared/components/applicability-section/effect-carousel-large.esm.js
+++ src/app/shared/components/applicability-section/effect-carousel-large.esm.js
@@ -1,4 +1,4 @@
-export default function CarouselSliderLarge({ swiper, on }) {
+export default function CarouselSlider({ swiper, on }) {
     on('beforeInit', () => {
         if (swiper.params.effect !== 'carousel') return;
         swiper.classNames.push(`${swiper.params.containerModifierClass}carousel`);
@@ -12,26 +12,35 @@ export default function CarouselSliderLarge({ swiper, on }) {
     });
     on('progress', () => {
         if (swiper.params.effect !== 'carousel') return;
-        const scaleStep = 0.185;
+        const scaleStep = 0.2;
         const zIndexMax = swiper.slides.length;
         for (let i = 0; i < swiper.slides.length; i += 1) {
             const slideEl = swiper.slides[i];
             const slideProgress = swiper.slides[i].progress;
             const absProgress = Math.abs(slideProgress);
             let modify = 1;
+            const zIndex = zIndexMax - Math.abs(Math.round(slideProgress));
+            let translateMultiplayer;
+
+            if (zIndex === 38) {
+                translateMultiplayer = 73.3;
+            } else {
+                translateMultiplayer = 79.5;
+            };
             if (absProgress > 1) {
-                modify = (absProgress - 1.6) *(0.04) + 1;
-            }
+                modify = (absProgress - 1) * 0.1 + 1;
+            };
+
             const opacityEls = slideEl.querySelectorAll(
                 '.swiper-carousel-animate-opacity',
             );
-            const translate = `${
-                slideProgress * modify * (-40) * (swiper.rtlTranslate ? 2: -2)
-            }%`;
+            const translate =
+
+                slideProgress * modify * translateMultiplayer * (swiper.rtlTranslate ? -1 : 1)
+            ;
 
             const scale = 1 - absProgress * scaleStep;
-            const zIndex = zIndexMax - Math.abs(Math.round(slideProgress));
-            slideEl.style.transform = `translateX(${translate}) scale(${scale})`;
+            slideEl.style.transform = `translateX(${translate}%) scale(${scale})`;
             slideEl.style.zIndex = zIndex;
             if (absProgress > 3) {
                 slideEl.style.opacity = 0;
